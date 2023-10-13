fe2f097a705fc3526ec14e26bf35f8e1b869d490
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-13 18:57:21 +0600

    create booking and service status enum

---
+++ src/enums/booking.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export enum ENUM_BOOKING_STATUS {
+  PENDING = 'pending',
+  ACCEPT = 'accept',
+  REJECT = 'reject',
+}
---
+++ src/enums/service.ts
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+export enum ENUM_SERVICE_STATUS {
+  AVAILABLE = 'available',
+  NOT_AVAILABLE = 'not_available',
+  UPCOMING = 'upcoming',
+}
