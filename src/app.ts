d556d1c6f54a37a9551e9a6420ce5e7417d0429c
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-10-14 15:37:30 +0600

    add localhost 3000 to cors origin

--- src/app.ts
+++ src/app.ts
@@ -13,7 +13,7 @@ import { ENUM_USER_ROLE } from './enums/user';
 
 const app: Application = express();
 
-app.use(cors());
+app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 app.use(cookieParser());
 
 //parser
