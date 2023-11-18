48538ddab9a796f610a35a42c275dc1155880801
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2023-11-18 12:56:32 +0600

    Add update and delete permission to user

--- src/app/modules/reviewAndRating/reviewAndRating.routes.ts
+++ src/app/modules/reviewAndRating/reviewAndRating.routes.ts
@@ -23,12 +23,12 @@ router.get('/:id', ReviewAndRatingController.getSingleReviewAndRating);
 
 router.patch(
   '/:id',
-  auth(ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
   ReviewAndRatingController.updateReviewAndRating
 );
 router.delete(
   '/:id',
-  auth(ENUM_USER_ROLE.ADMIN),
+  auth(ENUM_USER_ROLE.ADMIN, ENUM_USER_ROLE.USER),
   ReviewAndRatingController.deleteReviewAndRating
 );
 
