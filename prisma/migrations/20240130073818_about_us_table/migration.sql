abae395e77535c098758a431486a5beec0e53c25
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-01-30 13:38:36 +0600

    Create a prisma schema for AboutUs table

---
+++ prisma/migrations/20240130073818_about_us_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "about_us" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "about_us_pkey" PRIMARY KEY ("id")
+);
