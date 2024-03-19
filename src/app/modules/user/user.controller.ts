4dfb3a1af0d2e2dfe71e9a70bc866ee8279d2350
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-03-19 12:32:40 +0600

    check query data is pass

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -52,6 +52,8 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 
 const getAllUsers = catchAsync(async (req: Request, res: Response) => {
   const user = req.user;
+  const query = req.query;
+  console.log(query);
   const result = await UserService.getAllUsers(user);
 
   sendResponse(res, {
