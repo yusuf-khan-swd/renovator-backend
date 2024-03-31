4dc182d49f5b29888dfef6e652c58ea02c258916
yusuf-khan-swd <yusufkhan.swd@gmail.com>
2024-03-31 11:58:17 +0600

    Add a space after each route

--- src/app/modules/user/user.routes.ts
+++ src/app/modules/user/user.routes.ts
@@ -8,27 +8,32 @@ import { UserValidation } from './user.validation';
 const router = express.Router();
 
 router.get('/', auth(ENUM_USER_ROLE.SUPER_ADMIN), UserController.getAllUsers);
+
 router.get(
   '/admins',
   auth(ENUM_USER_ROLE.SUPER_ADMIN),
   UserController.getAllAdminUsers
 );
+
 router.get(
   '/normal-users',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   UserController.getAllNormalUsers
 );
+
 router.get(
   '/:id',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   UserController.getSingleUser
 );
+
 router.patch(
   '/:id',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
   validateRequest(UserValidation.updateUserZodValidation),
   UserController.updateUser
 );
+
 router.delete(
   '/:id',
   auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
